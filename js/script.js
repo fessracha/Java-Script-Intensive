a0e7b5c786e54a225485c76310273486016f6653
Eugene Shangin <fessracha@gmail.com>
2019-02-20 11:02:25 +0500

    lesson 2

--- js/script.js
+++ js/script.js
@@ -23,16 +23,19 @@ window.addEventListener('DOMContentLoaded', () => {
     open.addEventListener('click', openCart);
     close.addEventListener('click', closeCart);
 
-    goodsBtn.forEach( (btn, i) => {
+    goodsBtn.forEach((btn, i) => {
         btn.addEventListener('click', () => {
 
             let item = products[i].cloneNode(true),
                 trigger = item.querySelector('button'),
                 removeBtn = document.createElement('div'),
                 empty = cartWrapper.querySelector('.empty');
-            
+
             trigger.remove();
-            
+
+            showConfirm();
+            calcGoods(1);
+
             removeBtn.classList.add('goods__item-remove');
             removeBtn.innerHTML = '&times';
             item.appendChild(removeBtn);
@@ -40,8 +43,78 @@ window.addEventListener('DOMContentLoaded', () => {
             cartWrapper.appendChild(item);
 
             if (empty) {
-                empty.remove();
+                empty.style.display = 'none';
             }
-        }); 
+
+            calcTotal();
+            removeFromCart();
+        });
     });
+
+    function sliceTitle() {
+        titles.forEach((item) => {
+            if (item.textContent.length < 70) {
+                return;
+            } else {
+                const str = `${item.textContent.slice(0,71)}...`;
+
+                item.textContent = str;
+            }
+        });
+    }
+
+    sliceTitle();
+
+    function showConfirm() {
+
+        confirm.style.display = 'block';
+        let counter = 100;  
+        const id = setInterval(frame, 10);
+
+        function frame() {
+            if(counter == 10) {
+                clearInterval(id);
+                confirm.style.display = 'none';
+            } else {
+                counter--;
+                confirm.style.opacity = `.${counter}`;
+                confirm.style.transform = `translateY(-${counter}px)`;
+            } 
+        }
+    }
+
+    function calcGoods(i) {
+        const items = cartWrapper.querySelectorAll('.goods__item');
+        badge.textContent = items.length + i;
+    }
+
+    function calcTotal() {
+        const prices = document.querySelectorAll('.cart__wrapper > .goods__item > .goods__price > span');
+        let total = 0;
+
+        prices.forEach((item) => {
+            total += +item.textContent;
+        })
+
+        totalCost.textContent = total;
+
+        return total;
+
+    }
+
+    function removeFromCart() {
+        const removeBtn = cartWrapper.querySelectorAll('.goods__item-remove'),
+              empty = cartWrapper.querySelector('.empty');
+        removeBtn.forEach((btn) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                calcGoods(0);
+
+                if(calcTotal() == 0) {
+                    empty.style.display = 'block';
+                }
+            });
+        });
+    }
+
 });
